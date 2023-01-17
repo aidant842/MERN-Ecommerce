e8fe686db005d97f910db6b32e3af45620237747
Aidan Thorpe <aidant842@gmail.com>
2023-01-17 17:19:52 +0000

    added product component, and HomeScreen screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 const App = () => {
     return (
         <>
             <Header></Header>
             <main className="py-3">
                 <Container>
-                    <h1>Welcome to ProShop</h1>
+                    <HomeScreen></HomeScreen>
                 </Container>
             </main>
             <Footer></Footer>
