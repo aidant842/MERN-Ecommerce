d228ffb28a7577452ff3bdc7f9277c14982bb4e8
Aidan Thorpe <aidant842@gmail.com>
2023-01-26 16:51:42 +0000

    started user authentication and authorization

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,26 @@
+import jwt from "jsonwebtoken";
+import asyncHandler from "express-async-handler";
+import User from "../models/userModel.js";
+
+const protect = asyncHandler(async (req, res, next) => {
+    let token;
+
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        try {
+            token = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.verify(token, process.env._JWT_SECRET);
+            console.log(decoded);
+            next();
+        } catch (error) {}
+    }
+
+    if (!token) {
+        res.status(401);
+        throw new Error("Not authorized, no token");
+    }
+});
+
+export { protect };
--- backend/server.js
+++ backend/server.js
@@ -3,17 +3,21 @@ import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 dotenv.config();
 connectDB();
 const app = express();
 
+app.use(express.json()); //allowing json data in the request body
+
 app.get("/", (req, res) => {
     res.send("Api is running......");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
